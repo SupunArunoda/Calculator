b52775c4095b6c765f0bea500a0a4c18f4d6d22c
Chanaka Dinesh <chanakadinesh06@gmail.com>
2015-08-13 10:51:31 +0530

    log function added

--- Qunit/calculator.js
+++ Qunit/calculator.js
@@ -5,3 +5,8 @@ function plus(a, b) {
 function minus(a, b) {
     return a - b;
 }
+
+
+function logs(a){
+	return Math.log10(a);
+}
--- Qunit/tests.js
+++ Qunit/tests.js
@@ -9,3 +9,13 @@ QUnit.test("Testing Plus function", function(assert) {
 QUnit.test("Testing Plus function with failing", function(assert) {
   assert.equal(5, plus(2, 2), "2 + 2 is 5");
 });
+
+
+QUnit.test("Logs function integer",function(assert){
+	assert.equal(2,logs(100),"log(100) is 2");
+});
+
+
+QUnit.test("Logs function decimal",function(assert){
+	assert.equal(-2,logs(0.01),"log(0.01) is -2");
+});
